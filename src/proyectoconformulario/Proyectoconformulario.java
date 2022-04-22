bdad9283a76ff5f625b82d3b04afb9c8b6e5d178
brahianbran <alexisbran33@gmail.com>
2022-04-21 19:47:51 -0500

    Proyecto

---
+++ src/proyectoconformulario/Proyectoconformulario.java
@@ -0,0 +1,14 @@
+
+package proyectoconformulario;
+
+
+public class Proyectoconformulario {
+
+    
+    public static void main(String[] args)
+    {
+        frmventana1 form1=new frmventana1();
+        form1.setVisible(true);
+    }
+    
+}
